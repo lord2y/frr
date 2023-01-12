8cd2860849c0ba11e55af9a2d3889e0f195f5650
Alessandro Ratti <alessandro.ratti@exoscale.ch>
2023-01-12 18:39:17 +0100

    Let's try to print route_table->info

--- zebra/zebra_rnh.c
+++ zebra/zebra_rnh.c
@@ -573,6 +573,8 @@ zebra_rnh_resolve_nexthop_entry(struct zebra_vrf *zvrf, afi_t afi,
 	*prn = NULL;
 
 	route_table = zvrf->table[afi][rnh->safi];
+	zlog_info("table_id: %u", route_table_get_info(route_table));
+
 	if (!route_table)
 		return NULL;
 
